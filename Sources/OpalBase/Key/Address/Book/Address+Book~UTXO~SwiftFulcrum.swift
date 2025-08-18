9bd1e4a16d00c7abb31ed84eb804cbaa25fb6008
Coda Beatrix <coda@58opals.com>
2025-08-19 00:24:40 +0900

    2025-08-19-00-24-33

--- Sources/OpalBase/Key/Address/Book/Address+Book~UTXO~SwiftFulcrum.swift
+++ Sources/OpalBase/Key/Address/Book/Address+Book~UTXO~SwiftFulcrum.swift
@@ -5,18 +5,27 @@ import SwiftFulcrum
 
 extension Address.Book {
     public func refreshUTXOSet(fulcrum: Fulcrum) async throws {
-        let operation = { [self] in
-            var updatedUTXOs = [Transaction.Output.Unspent]()
+        let operation = { [self] () async throws -> Void in
+            let entries = receivingEntries + changeEntries
             
-            for entry in (receivingEntries + changeEntries) {
-                let newUTXOs = try await entry.address.fetchUnspentTransactionOutputs(fulcrum: fulcrum)
-                let orderedUTXOs = newUTXOs.map {
-                    Transaction.Output.Unspent(value: $0.value,
-                                               lockingScript: $0.lockingScript,
-                                               previousTransactionHash: $0.previousTransactionHash,
-                                               previousTransactionOutputIndex: $0.previousTransactionOutputIndex)
+            let updatedUTXOs = try await withThrowingTaskGroup(of: [Transaction.Output.Unspent].self) { group in
+                for entry in entries {
+                    group.addTask {
+                        let newUTXOs = try await entry.address.fetchUnspentTransactionOutputs(fulcrum: fulcrum)
+                        return newUTXOs.map {
+                            Transaction.Output.Unspent(value: $0.value,
+                                                       lockingScript: $0.lockingScript,
+                                                       previousTransactionHash: $0.previousTransactionHash,
+                                                       previousTransactionOutputIndex: $0.previousTransactionOutputIndex)
+                        }
+                    }
                 }
-                updatedUTXOs.append(contentsOf: orderedUTXOs)
+                
+                var collected: [Transaction.Output.Unspent] = .init()
+                for try await utxos in group {
+                    collected.append(contentsOf: utxos)
+                }
+                return collected
             }
             
             clearUTXOs()
