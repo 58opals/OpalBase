d9bb1fb6c4b97b9c59f0daf73f933114891f57a3
Coda Beatrix <coda@58opals.com>
2025-10-29 15:17:46 +0900

    2025-10-29-15-17-42

--- Tests/OpalBaseTests/Network/Network+FulcrumSession~Subscription~API+Tests.swift
+++ Tests/OpalBaseTests/Network/Network+FulcrumSession~Subscription~API+Tests.swift
@@ -3,8 +3,8 @@ import Testing
 import SwiftFulcrum
 @testable import OpalBase
 
-@Suite("Network.FulcrumSession State", .tags(.network, .integration))
-struct NetworkFulcrumSessionStateTests {
+@Suite("Network.FulcrumSession Subscription API", .tags(.network, .integration))
+struct NetworkFulcrumSessionSubscriptionAPITests {
     private static let healthyServerAddress = URL(string: "wss://bch.imaginary.cash:50004")!
     private static let sampleAddress = "bitcoincash:qpm2qsznhks23z7629mms6s4cwef74vcwvy22gdx6a"
 }
