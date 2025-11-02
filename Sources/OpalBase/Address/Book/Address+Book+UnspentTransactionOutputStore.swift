04096a84544a6da4ca85e7942bcbdcd14ca0688c
Coda Beatrix <coda@58opals.com>
2025-11-02 17:43:18 +0900

    2025-11-02-17-43-11

--- Sources/OpalBase/Address/Book/Address+Book+UnspentTransactionOutputStore.swift
+++ Sources/OpalBase/Address/Book/Address+Book+UnspentTransactionOutputStore.swift
@@ -4,61 +4,107 @@ import Foundation
 
 extension Address.Book {
     public struct UnspentTransactionOutputStore {
-        private var unspentTransactionOutputs: Set<Transaction.Output.Unspent>
+        private var unspentTransactionOutputsByLockingScript: [Data: Set<Transaction.Output.Unspent>]
         
         init() {
-            self.unspentTransactionOutputs = .init()
+            self.unspentTransactionOutputsByLockingScript = .init()
         }
         
         mutating func add(_ unspentTransactionOutput: Transaction.Output.Unspent) {
-            unspentTransactionOutputs.insert(unspentTransactionOutput)
+            store(unspentTransactionOutput)
         }
         
         mutating func add(_ unspentTransactionOutputs: [Transaction.Output.Unspent]) {
-            self.unspentTransactionOutputs.formUnion(unspentTransactionOutputs)
+            guard !unspentTransactionOutputs.isEmpty else {
+                return
+            }
+            
+            for unspentTransactionOutput in unspentTransactionOutputs {
+                store(unspentTransactionOutput)
+            }
         }
         
         mutating func replace(with unspentTransactionOutputs: Set<Transaction.Output.Unspent>) {
-            self.unspentTransactionOutputs = unspentTransactionOutputs
+            unspentTransactionOutputsByLockingScript = unspentTransactionOutputs.reduce(into: [Data: Set<Transaction.Output.Unspent>]()) { result, unspent in
+                result[unspent.lockingScript, default: []].insert(unspent)
+            }
         }
         
         mutating func replace(for address: Address, with unspentTransactionOutputs: [Transaction.Output.Unspent]) {
             let lockingScript = address.lockingScript.data
-            let existingMatches = self.unspentTransactionOutputs.filter { $0.lockingScript == lockingScript }
-            if !existingMatches.isEmpty {
-                self.unspentTransactionOutputs.subtract(existingMatches)
-            }
-            if !unspentTransactionOutputs.isEmpty {
-                self.unspentTransactionOutputs.formUnion(unspentTransactionOutputs)
+            let newUnspentTransactionOutputs = Set(unspentTransactionOutputs)
+            
+            if newUnspentTransactionOutputs.isEmpty {
+                unspentTransactionOutputsByLockingScript.removeValue(forKey: lockingScript)
+            } else {
+                unspentTransactionOutputsByLockingScript[lockingScript] = newUnspentTransactionOutputs
             }
         }
         
         mutating func remove(_ unspentTransactionOutput: Transaction.Output.Unspent) {
-            unspentTransactionOutputs.remove(unspentTransactionOutput)
+            discard(unspentTransactionOutput)
         }
         
         mutating func remove(_ unspentTransactionOutputs: [Transaction.Output.Unspent]) {
-            self.unspentTransactionOutputs.subtract(unspentTransactionOutputs)
+            let removals = Set(unspentTransactionOutputs)
+            guard !removals.isEmpty else {
+                return
+            }
+            
+            for removal in removals {
+                discard(removal)
+            }
         }
         
         mutating func clear() {
-            unspentTransactionOutputs.removeAll()
+            unspentTransactionOutputsByLockingScript.removeAll()
         }
         
         func listUnspentTransactionOutputs() -> Set<Transaction.Output.Unspent> {
-            unspentTransactionOutputs
+            allUnspentTransactionOutputs
         }
         
         func sorted(by areInIncreasingOrder: (Transaction.Output.Unspent, Transaction.Output.Unspent) -> Bool)
         -> [Transaction.Output.Unspent] {
-            unspentTransactionOutputs.sorted(by: areInIncreasingOrder)
+            allUnspentTransactionOutputs.sorted(by: areInIncreasingOrder)
         }
         
         func findUnspentTransactionOutput(matching input: Transaction.Input) -> Transaction.Output.Unspent? {
-            unspentTransactionOutputs.first(where: { unspent in
-                unspent.previousTransactionHash == input.previousTransactionHash &&
-                unspent.previousTransactionOutputIndex == input.previousTransactionOutputIndex
-            })
+            for unspentTransactionOutputs in unspentTransactionOutputsByLockingScript.values {
+                if let match = unspentTransactionOutputs.first(where: { unspent in
+                    unspent.previousTransactionHash == input.previousTransactionHash &&
+                    unspent.previousTransactionOutputIndex == input.previousTransactionOutputIndex
+                }) {
+                    return match
+                }
+            }
+            
+            return nil
+        }
+        
+        private var allUnspentTransactionOutputs: Set<Transaction.Output.Unspent> {
+            unspentTransactionOutputsByLockingScript.values.reduce(into: Set<Transaction.Output.Unspent>()) { result, unspentTransactionOutputs in
+                result.formUnion(unspentTransactionOutputs)
+            }
+        }
+        
+        private mutating func store(_ unspentTransactionOutput: Transaction.Output.Unspent) {
+            unspentTransactionOutputsByLockingScript[unspentTransactionOutput.lockingScript, default: []]
+                .insert(unspentTransactionOutput)
+        }
+        
+        private mutating func discard(_ unspentTransactionOutput: Transaction.Output.Unspent) {
+            guard var indexedUnspentTransactionOutputs = unspentTransactionOutputsByLockingScript[unspentTransactionOutput.lockingScript] else {
+                return
+            }
+            
+            indexedUnspentTransactionOutputs.remove(unspentTransactionOutput)
+            
+            if indexedUnspentTransactionOutputs.isEmpty {
+                unspentTransactionOutputsByLockingScript.removeValue(forKey: unspentTransactionOutput.lockingScript)
+            } else {
+                unspentTransactionOutputsByLockingScript[unspentTransactionOutput.lockingScript] = indexedUnspentTransactionOutputs
+            }
         }
     }
 }
