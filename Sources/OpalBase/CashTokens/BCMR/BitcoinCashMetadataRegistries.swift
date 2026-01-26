6becc25140098a3da0818c6a701c7309d338dd4d
Coda Beatrix <coda@58opals.com>
2026-01-26 21:59:42 +0900

    2026-01-26-21-59-39

---
+++ Sources/OpalBase/CashTokens/BCMR/BitcoinCashMetadataRegistries.swift
@@ -0,0 +1,54 @@
+// BitcoinCashMetadataRegistries.swift
+
+import Foundation
+
+public enum BitcoinCashMetadataRegistries {}
+
+extension BitcoinCashMetadataRegistries {
+    public struct Registry: Codable, Sendable {
+        public let version: String?
+        public let registryIdentity: String?
+        public let identities: [String: [String: IdentitySnapshot]]?
+        
+        public init(
+            version: String?,
+            registryIdentity: String?,
+            identities: [String: [String: IdentitySnapshot]]?
+        ) {
+            self.version = version
+            self.registryIdentity = registryIdentity
+            self.identities = identities
+        }
+    }
+    
+    public struct IdentitySnapshot: Codable, Sendable {
+        public let name: String?
+        public let description: String?
+        public let token: TokenSnapshot?
+        public let uris: [String: String]?
+        
+        public init(
+            name: String?,
+            description: String?,
+            token: TokenSnapshot?,
+            uris: [String: String]?
+        ) {
+            self.name = name
+            self.description = description
+            self.token = token
+            self.uris = uris
+        }
+    }
+    
+    public struct TokenSnapshot: Codable, Sendable {
+        public let category: String?
+        public let symbol: String?
+        public let decimals: Int?
+        
+        public init(category: String?, symbol: String?, decimals: Int?) {
+            self.category = category
+            self.symbol = symbol
+            self.decimals = decimals
+        }
+    }
+}
